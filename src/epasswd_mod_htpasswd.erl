204924dc88097a4984c10f1b3cfff46410c0c50d
jean <jean@debian>
2014-07-10 12:16:03 +0200

    Update for manage users and groups

--- src/epasswd_mod_htpasswd.erl
+++ src/epasswd_mod_htpasswd.erl
@@ -29,8 +29,9 @@
 %%% epasswd callbacks
 %%%
 -export([init/1,
-	 auth/2,
-	 share_group/3]).
+         auth/2,
+         share_group/3, create_group/2, create_ingroup/3, create_user/3, delete_user/2,
+         delete_ingroup/2, delete_group/2, update_group/2, get_groups/1, start/2]).
 
 -define(NOT_LOADED, not_loaded(?LINE)).
 -on_load(load/0).
@@ -42,30 +43,34 @@
 init(Path) ->
     application:start(crypto),
     case file:open(Path, [read, raw, binary, {read_ahead, 512}]) of
-	{ok, File} ->
-	    Db = parse(file:read_line(File), File, gb_trees:empty()),
-	    #state{db=Db};
-	{error, Err} ->
-	    throw({error, Err})
+        {ok, File} ->
+            Db = parse(file:read_line(File), File, gb_trees:empty()),
+            #state{db=Db};
+        {error, Err} ->
+            throw({error, Err})
     end.
+
+-spec start(Opts :: term(), state()) -> {true | false, state()}.
+start(_Opts, State) ->
+    {true, State}.
 
 -spec auth({User :: iolist(), Passwd :: iolist()}, state()) -> {true | false, state()}.
 auth({User, Passwd}, State) when is_list(User) ->
     auth({list_to_binary(User), Passwd}, State);
 auth({User, Passwd}, #state{db=Db}=State) ->
     case gb_trees:lookup(User, Db) of
-	{value, Digest} ->
-	    P = if is_binary(Passwd) -> binary_to_list(Passwd);
-		   true -> Passwd
-		end,
-	    try validate_password(P, binary_to_list(Digest)) of
-		true -> {true, State};
-		false -> {false, State}
-	    catch _:Err ->
-		    throw({error, Err})
-	    end;
-	_ ->
-	    {false, State}
+        {value, Digest} ->
+            P = if is_binary(Passwd) -> binary_to_list(Passwd);
+                   true -> Passwd
+                end,
+            try validate_password(P, binary_to_list(Digest)) of
+                true -> {true, State};
+                false -> {false, State}
+            catch _:Err ->
+                      throw({error, Err})
+            end;
+        _ ->
+            {false, State}
     end.
 
 -spec share_group(User1 :: binary(), User2 :: binary(), state()) -> {true | false, state()}.
@@ -73,6 +78,38 @@ share_group(User, User, State) ->
     {true, State};
 share_group(_, _, State) ->
     {false, State}.
+
+-spec create_group(Group :: term(), state()) -> {true | false, state()}.
+create_group(_Group, State) ->
+    {false, State}.
+
+-spec delete_group(Group :: term(), state()) -> {true | false, state()}.
+delete_group(_Group, State) ->
+    {false, State}.
+
+-spec create_ingroup(User :: term(), Group :: term(), state()) -> {true | false, state()}.
+create_ingroup(_User, _Group, State) ->
+    {false, State}.
+
+-spec delete_ingroup(InGroup :: term(), state()) -> {true | false, state()}.
+delete_ingroup(_InGroup, State) ->
+    {false, State}.
+
+-spec update_group(Group :: term(), state()) -> {true | false, state()}.
+update_group(_Group, State) ->
+    {false, State}.
+
+-spec create_user(Jid :: term(), User :: term(), state()) -> {true | false, state()}.
+create_user(_Jid, _User, State) ->
+    {false, State}.
+
+-spec delete_user(User :: term(), state()) -> {true | false, state()}.
+delete_user(_User, State) ->
+    {false, State}.
+
+-spec get_groups(state()) -> {term(), state()}.
+get_groups(State) ->
+    {[], State}.
 
 %%%
 %%% NIF
@@ -86,10 +123,10 @@ validate_password(_, _) ->
 %%%
 parse({ok, Line}, File, Acc) ->
     case binary:split(binary:part(Line, 0, byte_size(Line)-1), [<<":">>]) of
-	[User, Passwd] ->
-	    parse(file:read_line(File), File, gb_trees:insert(User, Passwd, Acc));
-	_ ->
-	    throw({invalid_db_entry, Line})
+        [User, Passwd] ->
+            parse(file:read_line(File), File, gb_trees:insert(User, Passwd, Acc));
+        _ ->
+            throw({invalid_db_entry, Line})
     end;
 parse(eof, _, Acc) ->
     Acc;
@@ -98,13 +135,13 @@ parse({error, Err}, _, _) ->
 
 load() ->
     PrivDir = case code:priv_dir(?MODULE) of
-		  {error, _} ->
-		      EbinDir = filename:dirname(code:which(?MODULE)),
-		      AppPath = filename:dirname(EbinDir),
-		      filename:join(AppPath, "priv");
-		  Path ->
-		      Path
-	      end,
+                  {error, _} ->
+                      EbinDir = filename:dirname(code:which(?MODULE)),
+                      AppPath = filename:dirname(EbinDir),
+                      filename:join(AppPath, "priv");
+                  Path ->
+                      Path
+              end,
     erlang:load_nif(filename:join(PrivDir, "htpasswd"), 0).
 
 not_loaded(Line) ->
--- src/epasswd_mod_xmpp.erl
+++ src/epasswd_mod_xmpp.erl
@@ -26,33 +26,86 @@
 -behaviour(epasswd).
 
 -export([init/1,
-		 auth/2, share_group/3]).
+         auth/2, share_group/3, create_group/2, create_ingroup/3,
+         create_user/3, delete_user/2, delete_ingroup/2, delete_group/2, get_groups/1, start/2, update_group/2]).
 
--record(state, {db, path}).
+-record(state, {db, table, lists_group, session}).
 -type state() :: #state{}.
 
 -spec init([]) -> state().
 init([]) ->
-	F = fun() ->
-			mnesia:all_keys(occi_user)
-		end,
-	Db_Tuple = mnesia:transaction(F),
-	Db = element(2, Db_Tuple),
-	#state{db = Db}.
+    F = fun() ->
+                mnesia:all_keys(occi_user)
+        end,
+    Table = ets:new(table_user, []),
+    Db_Tuple = mnesia:transaction(F),
+    Db = element(2, Db_Tuple),
+    #state{db = Db, table = Table, lists_group=[]}.
+
+-spec start(Opts :: term(), state()) -> {true | false, state()}.
+start(Opts, State) ->
+    {true, State#state{session=Opts}}.
 
 -spec auth({User1 :: binary(), User2 :: binary()}, state()) -> {true | false, state()}.
 auth({_User1, User2}, #state{db=Db}=State) ->
-	case lists:member(User2, Db) of
-		true -> {false, State};
-		false -> {true, State}
-	end.
+    case lists:member(User2, Db) of
+        true -> {true, State};
+        false -> {false, State}
+    end.
 
 -spec share_group(User1 :: binary(), User2 :: binary(), state()) -> {true | false, state()}.
 share_group(User, User, State) ->
     {true, State};
 share_group(_User1, User2, State) ->
-	Groups = element(2, User2),		
-	if 
-				is_binary(Groups) == true -> {true, State};
-				Groups == undefined -> {false, State}
-	end.
+    Groups = element(2, User2),		
+    if 
+        is_binary(Groups) == true -> {true, State};
+        Groups == undefined -> {false, State}
+    end.
+
+-spec create_group(Group :: term(), state()) -> {true | false, state()}.
+create_group(Group, #state{lists_group = Lgroup}=State) ->
+    Lgroup1 = Lgroup++Group,
+    {true, State#state{lists_group=Lgroup1}}.
+
+-spec delete_group(Group :: term(), state()) -> {true | false, state()}.
+delete_group(_Group, State) ->
+    {true, State}.
+
+-spec create_ingroup(User :: term(), Group :: term(), state()) -> {true | false, state()}.
+create_ingroup(User, Group, #state{table = Table, lists_group = Lgroup, session=Session}=State) ->
+    case ets:member(Table, User) of            
+        true -> case lists:member(Group, Lgroup) of
+                    true -> 
+                        LNick = ets:lookup(Table, User),
+                        TNick = lists:last(LNick),
+                        Nick = element(2, TNick),
+                        Iq = exmpp_client_roster:set_item(User, [Group], Nick),
+                        exmpp_session:send_packet(Session, Iq),
+                        ets:insert(Table, {User, Group}),
+                        {true, State};
+                    false -> {false, State}
+                end;
+        false -> {false, State}
+    end.
+
+-spec delete_ingroup(InGroup :: term(), state()) -> {true | false, state()}.
+delete_ingroup(_InGroup, State) ->
+    {true, State}.
+
+-spec update_group(Group :: term(), state()) -> {true | false, state()}.
+update_group(_Group, State) ->
+    {true, State}.
+
+-spec create_user(Jid :: term(), User :: term(), state()) -> {true | false, state()}.
+create_user(Jid, User, #state{table = Table}=State) ->
+    ets:insert(Table, {Jid, User}),
+    {true, State}.
+
+-spec delete_user(User :: term(), state()) -> {true | false, state()}.
+delete_user(_User, State) ->
+    {true, State}.
+
+-spec get_groups(state()) -> {term(), state()}.
+get_groups(#state{lists_group=Lgroup}=State) ->
+    {Lgroup, State}.
